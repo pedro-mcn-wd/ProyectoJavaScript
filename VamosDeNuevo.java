84fd95b1956940c68e1f30b209146f682d915a4e
Pedro <pedromanuelcarrascoso@gmail.com>
2022-02-16 13:43:49 +0000

    Otra actualizacion del ProyectoJavaScript

---
+++ VamosDeNuevo.java
@@ -0,0 +1,9 @@
+public class VamosDeNuevo {
+
+		//Tu primer programa con una llamada a main().
+		//Imprime el mensaje indicado en System.out.println a la ventana de la terminal.
+	public static void main(String args[]) {
+		
+		System.out.println("Aperitivo, comida y postre. Volvemos a empezar!!");
+	}
+}
