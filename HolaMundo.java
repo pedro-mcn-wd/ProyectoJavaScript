83fdb78169dd72ee63f068d2e68894a33fa04432
Pedro <pedromanuelcarrascoso@gmail.com>
2022-02-16 13:25:07 +0000

    Actualizacion del ProyectoJavaScript

---
+++ HolaMundo.java
@@ -0,0 +1,9 @@
+public class HolaMundo {
+
+		//Tu primer programa con una llamada a main().
+		//Imprime "Hola mundo" a la ventana de la terminal.
+	public static void main(String args[]) {
+		
+		System.out.println("Hola mundo");
+	}
+}
