bbc55e447d679161dcc2b0762ace96eaaac46ffc
Pedro <pedromanuelcarrascoso@gmail.com>
2022-02-22 11:00:46 +0000

    Colores definidos

--- HolaMundo.java
+++ HolaMundo.java
@@ -3,7 +3,9 @@ public class HolaMundo {
 		//Tu primer programa con una llamada a main().
 		//Imprime "Hola mundo" a la ventana de la terminal.
 	public static void main(String args[]) {
-		
+		String naranja = "\033[33m";
+		String azul = "\033[34m";
+
 		System.out.println("Hola mundo");
 		System.out.println("Aquí estamos  probando Git.");
 	}
