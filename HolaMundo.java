4c2e00f2d296d8e7e5d0ca77f51029fc5a118f00
Pedro <pedromanuelcarrascoso@gmail.com>
2022-02-22 11:04:06 +0000

    Colores definidos

--- HolaMundo.java
+++ HolaMundo.java
@@ -6,7 +6,7 @@ public class HolaMundo {
 		String naranja = "\033[33m";
 		String azul = "\033[34m";
 
-		System.out.println("Hola mundo");
-		System.out.println("Aquí estamos  probando Git.");
+		System.out.println(naranja + "Hola mundo");
+		System.out.println(azul + "Aquí estamos  probando Git.");
 	}
 }
